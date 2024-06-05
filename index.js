901b3241b4ccbb491066795fa979077713b5d814
jehu <jehugarrix@gmail.com>
2024-06-05 17:16:58 -0600

    primer commit

---
+++ index.js
@@ -0,0 +1,14 @@
+// Función para sumar dos números
+function sumarNumeros(num1, num2) {
+    return num1 + num2;
+}
+
+// Definimos dos números
+let numero1 = 5;
+let numero2 = 3;
+
+// Llamamos a la función de suma y almacenamos el resultado
+let resultado = sumarNumeros(numero1, numero2);
+
+// Imprimimos el resultado en la consola
+console.log("La suma de " + numero1 + " y " + numero2 + " es: " + resultado);
